217cf8df6db12ea464cb43452f4da02f33f52dd9
afiqahk <7786945+afiqahk@users.noreply.github.com>
2024-06-24 00:52:58 +0800

    Add project files

---
+++ MultithreadConsoleApp/Receiver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultithreadConsoleApp
+{
+    internal static class Receiver
+    {
+        internal static void RunReceiver() 
+        { }
+    }
+}
---
+++ MultithreadConsoleApp/Sender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultithreadConsoleApp
+{
+    internal static class Sender
+    {
+        internal static void RunSender()
+        {
+
+        }
+    }
+}
